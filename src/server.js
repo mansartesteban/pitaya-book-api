820cec202653548d131fe48392cc242c3f14e9c4
Esteban Mansart <esteban.mansart@gmail.com>
2026-03-25 23:12:37 +0100

    fix cors

--- src/server.js
+++ src/server.js
@@ -95,7 +95,11 @@ app.register(multipart, {
 })
 
 await app.register(cors, {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://pitaya-photo.com",
+    "https://www.pitaya-photo.com",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 })
